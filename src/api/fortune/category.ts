0d4a08059a1a30ba2e408aa067f9c11e9d8556ff
work.chi.zhang@gmail.com <work.chi.zhang@gmail.com>
2025-02-15 03:36:41 +0800

    feat: 分类开发

--- src/api/fortune/category.ts
+++ src/api/fortune/category.ts
@@ -1,6 +1,7 @@
 import { http } from "@/utils/http";
+import { Tree } from "@/utils/tree";
 
-export interface CategoryVo {
+export interface CategoryVo extends Tree {
   categoryId?: number;
   categoryName?: string;
   categoryType?: number;
@@ -9,14 +10,53 @@ export interface CategoryVo {
   sort?: number;
   remark?: string;
   enable?: boolean;
-  recycleBin?: boolean;
-  children?: CategoryVo;
 }
+
+export interface CategoryQuery extends BasePageQuery {
+  bookId?: number;
+  categoryType?: number;
+  recycleBin?: boolean;
+  categoryName?: string;
+}
+
+export const getCategoryPageApi = (params: CategoryQuery) => {
+  return http.request<ResponseData<PageDTO<CategoryVo>>>(
+    "get",
+    `/fortune/category/getPage`,
+    {
+      params
+    }
+  );
+};
 
 // 分类
 export const getEnableCategoryList = (bookId: number, billType: number) => {
   return http.request<ResponseData<Array<CategoryVo>>>(
     "get",
     `/fortune/category/${bookId}/${billType}/getEnableList`
+  );
+};
+
+export const categoryEnableApi = (bookId: number, categoryId: number) => {
+  return http.request<ResponseData<any>>(
+    "patch",
+    `/fortune/category/${bookId}/${categoryId}/enable`
+  );
+};
+
+export const categoryDisableApi = (bookId: number, categoryId: number) => {
+  return http.request<ResponseData<any>>(
+    "patch",
+    `/fortune/category/${bookId}/${categoryId}/disable`
+  );
+};
+
+export const moveCategory2RecycleBinApi = (
+  bookId: number,
+  categoryId: number
+) => {
+  return http.request<ResponseData<any>>(
+    "patch",
+    `/fortune/category/${bookId}/${categoryId}/moveToRecycleBin`
   );
 };
--- src/views/fortune/payee/utils/hook.tsx
+++ src/views/fortune/payee/utils/hook.tsx
@@ -222,7 +222,7 @@ export function useHook() {
   ];
 
   return {
-    searchFormParams: searchForm,
+    searchForm,
     loading,
     title,
     columns,
