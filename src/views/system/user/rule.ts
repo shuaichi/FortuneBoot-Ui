2bf1d21429c63898f3f8f2a4b106227a16dde525
work.chi.zhang@gmail.com <work.chi.zhang@gmail.com>
2025-02-17 13:54:24 +0800

    feat: 密码长度校验

--- src/views/system/user/rule.ts
+++ src/views/system/user/rule.ts
@@ -2,12 +2,32 @@ import { reactive } from "vue";
 import type { FormRules } from "element-plus";
 import { isPhone, isEmail } from "@pureadmin/utils";
 
+/** 密码正则（密码格式应为8-18位数字、字母、符号的任意两种组合） */
+export const REGEXP_PWD =
+  /^(?![0-9]+$)(?![a-z]+$)(?![A-Z]+$)(?!([^(0-9a-zA-Z)]|[()])+$)(?!^.*[\u4E00-\u9FA5].*$)([^(0-9a-zA-Z)]|[()]|[a-z]|[A-Z]|[0-9]){8,18}$/;
 /** 自定义表单规则校验 */
 export const formRules = reactive(<FormRules>{
   username: [{ required: true, message: "用户名为必填项", trigger: "blur" }],
   nickname: [{ required: true, message: "昵称为必填项", trigger: "blur" }],
   roleId: [{ required: true, message: "角色为必选项", trigger: "blur" }],
-  password: [{ required: true, message: "密码为必填项", trigger: "blur" }],
+  password: [
+    {
+      required: true,
+      message: "密码为必填项",
+      validator: (rule, value, callback) => {
+        if (value === "") {
+          callback(new Error("请输入密码"));
+        } else if (!REGEXP_PWD.test(value)) {
+          callback(
+            new Error("密码格式应为8-18位数字、字母、符号的任意两种组合")
+          );
+        } else {
+          callback();
+        }
+      },
+      trigger: "blur"
+    }
+  ],
   phone: [
     {
       validator: (rule, value, callback) => {
