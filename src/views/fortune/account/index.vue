74ac482153e79b25d8c88f01d8cce36c75895b40
work.chi.zhang@gmail.com <work.chi.zhang@gmail.com>
2025-02-22 20:43:02 +0800

    feat: 细节修复

--- src/views/fortune/account/index.vue
+++ src/views/fortune/account/index.vue
@@ -1,125 +1,135 @@
 <template>
-  <div class="full-width-container">
-    <el-radio-group
-      v-model="searchForm.accountType"
-      size="large"
-      class="full-width-group"
-      @change="onSearch(barRef)"
-    >
-      <el-radio-button :label="1" class="quarter-width">活 期</el-radio-button>
-      <el-radio-button :label="2" class="quarter-width">信 用</el-radio-button>
-      <el-radio-button :label="3" class="quarter-width">资 产</el-radio-button>
-      <el-radio-button :label="4" class="quarter-width">贷 款</el-radio-button>
-    </el-radio-group>
-  </div>
   <div>
-    <el-form
-      ref="formRef"
-      :inline="true"
-      :model="searchForm"
-      class="search-form bg-bg_color w-[99/100] pl-8 pt-[12px]"
-    >
-      <el-form-item label="所属分组：" prop="groupId">
-        <el-select
-          v-model="searchForm.groupId"
-          placeholder="请选择分组"
-          class="!w-[180px]"
-          filterable
+    <div class="full-width-container">
+      <el-radio-group
+        v-model="searchForm.accountType"
+        size="large"
+        class="full-width-group"
+        @change="onSearch(barRef)"
+      >
+        <el-radio-button :label="1" class="quarter-width"
+          >活 期</el-radio-button
         >
-          <el-option
-            v-for="item in groupOptions"
-            :key="item.groupId"
-            :label="item.groupName"
-            :value="item.groupId"
+        <el-radio-button :label="2" class="quarter-width"
+          >信 用</el-radio-button
+        >
+        <el-radio-button :label="3" class="quarter-width"
+          >资 产</el-radio-button
+        >
+        <el-radio-button :label="4" class="quarter-width"
+          >贷 款</el-radio-button
+        >
+      </el-radio-group>
+    </div>
+    <div>
+      <el-form
+        ref="formRef"
+        :inline="true"
+        :model="searchForm"
+        class="search-form bg-bg_color w-[99/100] pl-8 pt-[12px]"
+      >
+        <el-form-item label="所属分组：" prop="groupId">
+          <el-select
+            v-model="searchForm.groupId"
+            placeholder="请选择分组"
+            class="!w-[180px]"
+            filterable
+          >
+            <el-option
+              v-for="item in groupOptions"
+              :key="item.groupId"
+              :label="item.groupName"
+              :value="item.groupId"
+            />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="账户名称：" prop="accountName">
+          <el-input
+            v-model="searchForm.accountName"
+            placeholder="请输入账本名称"
+            clearable
+            class="!w-[200px]"
           />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="账户名称：" prop="accountName">
-        <el-input
-          v-model="searchForm.accountName"
-          placeholder="请输入账本名称"
-          clearable
-          class="!w-[200px]"
-        />
-      </el-form-item>
-      <!-- 其他搜索条件 -->
-      <el-form-item>
-        <el-button
-          type="primary"
-          :icon="useRenderIcon(Search)"
-          :loading="loading"
-          @click="
-            () => {
-              onSearch(barRef);
-            }
-          "
-        >
-          搜索
-        </el-button>
-        <el-button :icon="useRenderIcon(Refresh)" @click="resetForm()">
-          重置
-        </el-button>
-      </el-form-item>
-    </el-form>
-    <PureTableBar
-      ref="barRef"
-      title="账户管理"
-      :columns="columns"
-      @refresh="onSearch"
-    >
-      <template #buttons>
-        <el-button
-          type="primary"
-          :icon="useRenderIcon(AddFill)"
-          @click="openDialog('add')"
-        >
-          新增账户
-        </el-button>
-      </template>
-      <!-- 表格列绑定账户字段 -->
-      <template v-slot="{ size, dynamicColumns }">
-        <pure-table
-          border
-          align-whole="center"
-          showOverflowTooltip
-          table-layout="auto"
-          :loading="loading"
-          :size="size"
-          adaptive
-          :data="dataList"
-          :columns="dynamicColumns"
-          :pagination="pagination"
-          :paginationSmall="size === 'small'"
-          :header-cell-style="{
-            background: 'var(--el-table-row-hover-bg-color)',
-            color: 'var(--el-text-color-primary)'
-          }"
-          @page-size-change="handleSizeChange"
-          @page-current-change="handleCurrentChange"
-        >
-          <!-- 操作列 -->
-          <template #operation="{ row }">
-            <el-button link type="primary" @click="openDialog('edit', row)">
-              编辑
-            </el-button>
-            <el-popconfirm
-              :title="`确认将【${row.accountName}】账户移入回收站？`"
-              @confirm="handleMove2RecycleBin(row)"
-            >
-              <template #reference>
-                <el-button link type="danger">回收站</el-button>
-              </template>
-            </el-popconfirm>
-          </template>
-        </pure-table>
-      </template>
-    </PureTableBar>
-    <account-form
-      v-model="modalVisible"
-      :type="opType"
-      :row="currentRow"
-      @success="onSearch"
-    />
+        </el-form-item>
+        <!-- 其他搜索条件 -->
+        <el-form-item>
+          <el-button
+            type="primary"
+            :icon="useRenderIcon(Search)"
+            :loading="loading"
+            @click="
+              () => {
+                onSearch(barRef);
+              }
+            "
+          >
+            搜索
+          </el-button>
+          <el-button :icon="useRenderIcon(Refresh)" @click="resetForm()">
+            重置
+          </el-button>
+        </el-form-item>
+      </el-form>
+      <PureTableBar
+        ref="barRef"
+        title="账户管理"
+        :columns="columns"
+        @refresh="onSearch"
+      >
+        <template #buttons>
+          <el-button
+            type="primary"
+            :icon="useRenderIcon(AddFill)"
+            @click="openDialog('add')"
+          >
+            新增账户
+          </el-button>
+        </template>
+        <!-- 表格列绑定账户字段 -->
+        <template v-slot="{ size, dynamicColumns }">
+          <pure-table
+            border
+            align-whole="center"
+            showOverflowTooltip
+            table-layout="auto"
+            :loading="loading"
+            :size="size"
+            adaptive
+            :data="dataList"
+            :columns="dynamicColumns"
+            :pagination="pagination"
+            :paginationSmall="size === 'small'"
+            :header-cell-style="{
+              background: 'var(--el-table-row-hover-bg-color)',
+              color: 'var(--el-text-color-primary)'
+            }"
+            @page-size-change="handleSizeChange"
+            @page-current-change="handleCurrentChange"
+          >
+            <!-- 操作列 -->
+            <template #operation="{ row }">
+              <el-button link type="primary" @click="openDialog('edit', row)">
+                编辑
+              </el-button>
+              <el-popconfirm
+                :title="`确认将【${row.accountName}】账户移入回收站？`"
+                @confirm="handleMove2RecycleBin(row)"
+              >
+                <template #reference>
+                  <el-button link type="danger">回收站</el-button>
+                </template>
+              </el-popconfirm>
+            </template>
+          </pure-table>
+        </template>
+      </PureTableBar>
+      <account-form
+        v-model="modalVisible"
+        :type="opType"
+        :row="currentRow"
+        @success="onSearch"
+      />
+    </div>
   </div>
 </template>
 
@@ -139,7 +149,7 @@ import { message } from "@/utils/message";
 
 /** 组件name最好和菜单表中的router_name一致 */
 defineOptions({
-  name: "FortuneBook"
+  name: "FortuneAccount"
 });
 
 const opType = ref<"add" | "edit">("add");
@@ -163,7 +173,7 @@ const {
 } = useHook();
 
 onMounted(async () => {
-  const [groupRes] = await Promise.all([getEnableGroupList()]);
+  const groupRes = await getEnableGroupList();
   if (groupRes.data.length === 0) {
     message("请先启用或创建分组");
   }
